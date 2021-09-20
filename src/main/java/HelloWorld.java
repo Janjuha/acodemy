bd9b910f8494956531edf9ec9ae75407aada2ee1
krupskis <krupskis.jans@gmail.com>
2021-09-20 21:13:28 +0300

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        printHelloWorld("jas","uebas");
+    }
+
+    private static void printHelloWorld(String str1, String str2) {
+        System.out.println(str1 + str2);
+    }
+}
